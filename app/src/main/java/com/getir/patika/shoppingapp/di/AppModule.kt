fea0ef3f6830ec061dcae90f0e9313a6f2fc5292
mozandastan <m.ozandastan@gmail.com>
2024-04-23 23:09:42 +0300

    Unused lines checkout and fixed

--- app/src/main/java/com/getir/patika/shoppingapp/di/AppModule.kt
+++ app/src/main/java/com/getir/patika/shoppingapp/di/AppModule.kt
@@ -2,7 +2,6 @@ package com.getir.patika.shoppingapp.di
 
 import com.getir.patika.shoppingapp.data.repository.ProductRepository
 import com.getir.patika.shoppingapp.data.service.ApiService
-import com.getir.patika.shoppingapp.utils.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,10 +11,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
-    @Provides
-    @Singleton
-    fun provideBaseUrl() = Constants.BASE_URL
 
     @Provides
     @Singleton
